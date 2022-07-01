930a919ff01b9f3fcd786cdaae99bb10374b8a5f
Samreensaba <Samren_saba@yahoo.com>
2022-07-01 16:48:09 +0100

    update

--- weather/src/App.js
+++ weather/src/App.js
@@ -1,12 +1,63 @@
 
-import './App.css';
-import Homepage from './components/Homepage';
+import "bootstrap/dist/css/bootstrap.min.css"
+import Homepage from "./components/Homepage";
+import MyNavbar from './components/MyNavbar';
+import {useState} from 'react'
+
+const api={
+  key:"1c883548f975ddcaa3c1ba05a78d91b3",
+  base:"https://api.openweathermap.org/data/2.5/"
+}
 
 function App() {
+
+  const [query, setQuery]=useState('')
+  const [weather, setWeather]=useState({})
+
+  const search = async(evt)=>{
+    if(evt.key ==='Enter'){
+      const response = await fetch(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`)
+      const data = await response.json()
+      setWeather(data)
+      console.log(data)
+      setQuery('')
+    }
+  }
+
+  const dateBuilder = (d)=>{
+    let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+    let days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
+
+    let day= days[d.getDay()];
+    let date = d.getDate();
+    let month = months[d.getMonth()];
+    let year= d.getFullYear();
+
+    return`${day} ${date} ${month} ${year}`
+  }
   return (
-    <div className="App">
-      <Homepage/>
+    <div className="app">
+      <main>
+        <div className="search-box">
+          <input type="text" className="search-bar" placeholder="Search...." onChange={e=> {setQuery(e.target.value)}}
+          value={query}
+          onKeyPress={search}/>
+        </div>
+        {weather.main&&
+        <div>
+        <div className="location-box">
+          <div className="location">{weather.name}, {weather.sys.country}</div>
+          <div className="date">{dateBuilder(new Date())}</div>
+        </div>
+        </div> 
+          }
+        <div className="weather-box">
+          <div className="temp">{Math.round(weather.main.temp)}&#176; C</div>
+          <div className="weather">{weather.weather[0].main}</div>
+        </div>
+      </main>
     </div>
+    
   );
 }
 
--- weather/src/components/Homepage.jsx
+++ weather/src/components/Homepage.jsx
@@ -1,29 +1,51 @@
-import { useEffect } from "react"
-import { ListGroup } from "react-bootstrap";
+import {FormControl, Button, Form, Container, Row, Col} from "react-bootstrap"
+import { useState, useEffect } from "react";
+
 
 const Homepage = ()=>{
+  //const [cityname, setCityname]=useState('london')
+  //cosnt [result, setResult]=useState([])
 
-    useEffect(() => {
-        searchQuery();
-      }, []);
-    
-      const searchQuery = async () => {
-        try {
-          const response = await fetch(
-            "https://api.openweathermap.org/data/2.5/weather?q=london&appid=1c883548f975ddcaa3c1ba05a78d91b3"
-          );
-    
-          const data = await response.json();
-          const forecast =(data.main);
-        } catch (error) {
-          console.log(error);
-        }
-      };
+  /* useEffect(() => {
+    searchQuery();
+  }, [cityname]);
+
+  const newCity=(event)=>{
+    setCityname(event.target.value)
+  } */
+
+  const searchQuery = async (cityname) => {
+    try {
+      const response = await fetch(
+        `https://api.openweathermap.org/data/2.5/weather?q=${cityname}}&appid=1c883548f975ddcaa3c1ba05a78d91b3`
+      );
+
+      const data = await response.json();
+      const forecast =(data.main);
+    } catch (error) {
+      console.log(error);
+    }
+  };
     return(
-    
-        <ListGroup>
-          <ListGroup.Item></ListGroup.Item>
-        </ListGroup>
+      <Container>
+        
+      <Form inline className="mt-2">
+        <FormControl type="text" placeholder="Search"  style={{width:"50%", marginLeft:"25%"}} />
+        <Button variant="outline-success" className="ml-1">Search</Button>
+      </Form>
+      
+      
+
+      {/* <Row className="mt-2">
+        <Col md={6}>
+        <h2 className="text-center">Weather Forecast for ${cityname}</h2>
+        <p>{forecast.temp}</p>
+      
+      </Col>
+      </Row> */}
+      
+      
+    </Container>    
     )
 }
 export default Homepage
